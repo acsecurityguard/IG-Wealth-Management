ec1e290f0ac06e9b92609455b2b2be575f900d5e
Raul Rambo <raulhrjt@hotmail.com>
2018-12-04 21:05:01 -0600

    Initial Commit

---
+++ IQ Wealth Management/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  IQ Wealth Management
+//
+//  Created by Raul Rambo on 2018-12-04.
+//  Copyright © 2018 Raul Rambo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IQ Wealth Management/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  IQ Wealth Management
+//
+//  Created by Raul Rambo on 2018-12-04.
+//  Copyright © 2018 Raul Rambo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
